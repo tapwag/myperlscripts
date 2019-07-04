54fddba510064536aca5d9ac27572eaa5a5646cf
Maik Wagner <maiktapwagner@aol.com>
2019-07-04 08:51:59 +0200

    Removed typo

---
+++ 1_helloworld.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+# YouTube Video: https://www.youtube.com/watch?v=OVP-CqefrZY
+# Original Author: The Bad Tutorials
+
+use strict;
+use warnings;
+
+# Prints out the string "Hello World" and adds a new line
+print "Hello World!\n";
+
