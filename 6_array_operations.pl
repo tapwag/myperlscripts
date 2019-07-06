f7b4d41807a82696f8c53742b68bd5fa2a49fac0
Maik Wagner <maiktapwagner@aol.com>
2019-07-06 09:57:07 +0200

    One further tutorial

---
+++ 6_array_operations.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+@ranks=(1..10);
+@alphabets=(a..z);
+
+print "All ranks: @ranks\n";
+print "All alphabets: @alphabets\n";
+
+# Umwandlung in einen skalaren Wert und Ausgabe
+$size = @ranks;
+
+print "Size of ranks is $size\n";
